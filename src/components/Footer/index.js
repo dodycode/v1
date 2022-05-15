cf9874283331d0a290a806ae53952e240a5de608
dodycode <prasetyodody17@gmail.com>
2022-05-15 11:18:03 +0800

    remove unused component :P

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import "@sass/components/Footer.scss";
 const Footer = () => (
     <footer className="footer">
         <div className="footer__inner">
-            <span className="footer__inner__text">&copy; 2022 Dodycode. Designed by Seb Kay.</span>
+            <span className="footer__inner__text">&copy; 2022 Dodycode</span>
             <span className="footer__inner__text">Powered by React and Gatsby.js</span>
         </div>
     </footer>
