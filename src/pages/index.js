0675bf6966b2390cb588f423445c7d188eb6a154
Dody <prasetyodody17@gmail.com>
2022-11-18 17:07:47 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <div className="jumbotron">
       <h1>👋 Hi, I'm Dody.</h1>
       <p className="txt-large">
-        I'm a Full Stack Web Developer with more than 5 years of experience working professionally in Web Development, working with popular libraries and framework such as React JS, Vue JS, Next JS, Nuxt JS, Nest JS, Express and Laravel.
+        I'm a Web Developer based in Bali with more than 5 years of experience working professionally in Web Development, working with popular libraries and framework such as React JS, Vue JS, Next JS, Nuxt JS, Nest JS, Express and Laravel.
       </p>
       <p className="txt-large">
         I love typescript too, currently I use typescript for all of my new projects.
